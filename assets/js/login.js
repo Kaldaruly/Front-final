4b232cb11baceb70781b6e9d17bc96095b27397c
Kaldaruly <tastan2022@bk.ru>
2022-12-17 15:27:15 +0600

    fix: add js for login and register page

---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+let inputUser = document.getElementById("name");
+let inputEmail = document.getElementById("email");
+let inputPassword = document.getElementById("password");
+let submitBtn = document.querySelector(".btn");
+
+let user = JSON.parse(window.localStorage.getItem("userInfo"));
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  await fetch("http://localhost:8080/api/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    method: "POST",
+    body: JSON.stringify({
+      email: inputEmail.value,
+      password: inputPassword.value,
+    }),
+  }).then((res) => {
+    if (res.status === 500) {
+      if (
+        user.password !== inputPassword.value || 
+        user.email !== inputEmail.value
+      ) {
+        alert("Error can try again");
+      } else {
+        window.location.href = "/index.html";
+      }
+    }
+  });
+});
---
+++ assets/js/register.js
@@ -0,0 +1,33 @@
+let inputUser = document.getElementById('name')
+let inputEmail = document.getElementById('email')
+let inputPassword = document.getElementById('password')
+let submitBtn = document.querySelector('.btn');
+
+
+submitBtn.addEventListener('click', async(e) => {
+    e.preventDefault()
+
+    await fetch('http://localhost:8080/api/register', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+           username: inputUser.value,
+           email: inputEmail.value,
+           password: inputPassword.value
+        })
+    }).then(res => {
+        if (res.status === 500) {
+            window.location.href = '/login.html'
+
+            window.localStorage.setItem('userInfo', JSON.stringify({
+                username: inputUser.value,
+                email: inputEmail.value,
+                password: inputPassword.value
+            }))
+        }
+
+        console.log(res)
+    })
+})
