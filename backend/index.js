b6d3d018568cb2a4cb03eec278356953cd211bfc
MawinaABC <liskirzan@gmail.com>
2022-12-17 14:26:01 +0600

    feat: login and register api

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,20 @@ const app = express();
 const cors = require('cors');
 const db = require('./db/db')
 
+const authRegister = require('./user/register')
+const authLogin = require('./user/login')
+
 app.use(express());
 app.use(cors())
 
 // Connect database
 
 db();
+
+// Connect API
+
+app.use('/api/register', authRegister);
+app.use('/api/login', authLogin);
 
 const PORT = process.env.PORT || 8080;
 
